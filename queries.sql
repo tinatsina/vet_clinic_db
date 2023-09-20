de34e8fecc0dd0ad77ee4bdc5ae6517a2cb9e153
Tinaye Tsina <tinayetsina@outlook.com>
2023-09-20 10:52:15 +0800

    update data,schema and queries

--- queries.sql
+++ queries.sql
@@ -1,10 +1,51 @@
-/*Queries that provide answers to the questions from all projects.*/
+/*Queries that provide answers to the questions FROM all projects.*/
 
-select * from animals where name like '%mon';
-select * from animals where date_of_birth between '2016-01-01' and '2019-12-31';
-select * from animals where neutered is true and escape_attempts < 3;
-select date_of_birth from animals where name like 'Agumon' or name like 'Pikachu';
-select name,escape_attempts from animals where weight_kg > 10.5;
-select * from animals where neutered is true;
-select * from animals where not name='Gabumon';
-select * from animals where weight_kg >= 10.4 and weight_kg <= 17.3;
+SELECT * FROM animals WHERE name like '%mon';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT * FROM animals WHERE neutered is true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name like 'Agumon' or name like 'Pikachu';
+SELECT name,escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered is true;
+SELECT * FROM animals WHERE not name='Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/******************************************************************/
+/********************** QUERIES FROM DAY TWO **********************/
+/******************************************************************/
+
+-- Query 1
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+-- Query 2
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name like '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species is null;
+COMMIT;
+SELECT * FROM animals;
+
+-- Query 3
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+-- Query 4
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2020-01-01'::date;
+savepoint first_deletion;
+UPDATE animals SET weight_kg = (weight_kg*-1);
+ROLLBACK to savepoint first_deletion;
+UPDATE animals SET weight_kg = (weight_kg*-1) WHERE weight_kg < 0;
+COMMIT;
+
+-- Query 5
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, sum(escape_attempts) FROM animals GROUP BY neutered;
+
+-- Query 6
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2020-12-31' GROUP BY species;
