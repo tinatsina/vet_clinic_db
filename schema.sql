4bac84b537cdb49b9fb9b3b79fdc85a8cf4ae779
tinayetsina <tinayetsina@outlook.com>
2023-09-23 15:28:54 +0800

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -13,3 +13,30 @@ weight_kg real not null);
 /************************************/
 
 ALTER TABLE animals ADD species varchar(64);
+
+/************************************/
+/******* DAY THREE ASSIGNMENTS ******/
+/************************************/
+
+ create table owners (
+  id SERIAL,
+  full_name varchar(32),
+  age int,
+  PRIMARY KEY (id)
+);
+
+create table species (
+	id SERIAL,
+	name varchar(32),
+	PRIMARY KEY (id)
+);
+
+alter table animals
+add column species_id int references species(id)
+on delete cascade;
+select * from animals;
+
+alter table animals
+add column owner_id int references owners(id)
+on delete cascade;
+select * from animals;
